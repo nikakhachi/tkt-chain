711ff76f7f5f40e3f622bb29e13bb4298f710731
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-09-02 14:42:13 +0400

    add event creation fee

--- script/TKTChainFactory.sol
+++ script/TKTChainFactory.sol
@@ -13,11 +13,15 @@ contract TKTChainFactory is
     Ownable2StepUpgradeable,
     UUPSUpgradeable
 {
+    error InvalidFee();
+
     event EventCreated(
         address indexed eventAddress,
         address indexed owner,
         uint256 indexed timestamp
     );
+
+    uint256 public eventCreationFee;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -26,16 +30,22 @@ contract TKTChainFactory is
 
     /// @dev Upgradeable Contract Initializer
     /// @dev Can be called only once
-    function initialize() public initializer {
+    function initialize(uint256 _eventCreationFee) public initializer {
         __Ownable_init();
         __UUPSUpgradeable_init();
+        eventCreationFee = _eventCreationFee;
     }
 
     /// @notice Function for creating the event contract
-    function createEvent() external virtual returns (address) {
+    function createEvent() external payable virtual returns (address) {
+        if (msg.value < eventCreationFee) revert InvalidFee();
         TKTChainEvent e = new TKTChainEvent();
         emit EventCreated(address(e), msg.sender, block.timestamp);
         return address(e);
+    }
+
+    function updateFee(uint256 _newEventCreationFee) external onlyOwner {
+        eventCreationFee = _newEventCreationFee;
     }
 
     /// @dev Function for upgrading the contract
